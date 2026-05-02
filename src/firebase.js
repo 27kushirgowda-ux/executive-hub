29952f6d9766dc7d6dca2b213bc2df8bb3ea91b9
Kushi R <27kushi.r.gowda@gmail.com>
2026-05-02 16:40:50 +0530

    Secure environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAa2TW7hVEtuw-Bj0E98VssOGf6Ni2Y_pc",
-  authDomain: "interntrack-635c7.firebaseapp.com",
-  projectId: "interntrack-635c7",
-  storageBucket: "interntrack-635c7.firebasestorage.app",
-  messagingSenderId: "1025977528363",
-  appId: "1:1025977528363:web:056c3b4d1da7969eb7fa6e"
-};;
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
